98ce4eb26ed68cdff9cc4d2d4c76fef96d15e32e
Nia Mutiara <nmutiara@gmail.com>
2013-06-16 00:29:58 +0800

    Initial Commit

---
+++ GlowingLoadingIndicator/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  GlowingLoadingIndicator
+//
+//  Created by Nia Mutiara on 16/6/13.
+//  Copyright (c) 2013 Cat Cyborg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
